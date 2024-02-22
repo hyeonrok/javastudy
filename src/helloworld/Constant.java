8d68138fd80f3292faa670bef994d1b9c95d560c
hyeonrok <wjsgusfhr@naver.com>
2024-02-22 21:17:03 +0900

    학생평균과제1

---
+++ src/helloworld/Constant.java
@@ -0,0 +1,52 @@
+package helloworld;
+
+public class Constant {
+
+	/*배열 배우면 바꿔보기*/
+	private String korean;
+	private String math;
+	private String english;
+	
+	private String studentname1;
+	private String studentname2;
+	private String studentname3;
+	
+	public String getKorean() {
+		return korean;
+	}
+	public void setKorean(String korean) {
+		this.korean = korean;
+	}
+	public String getMath() {
+		return math;
+	}
+	public void setMath(String math) {
+		this.math = math;
+	}
+	public String getEnglish() {
+		return english;
+	}
+	public void setEnglish(String english) {
+		this.english = english;
+	}
+	public String getStudentname1() {
+		return studentname1;
+	}
+	public void setStudentname1(String studentname1) {
+		this.studentname1 = studentname1;
+	}
+	public String getStudentname2() {
+		return studentname2;
+	}
+	public void setStudentname2(String studentname2) {
+		this.studentname2 = studentname2;
+	}
+	public String getStudentname3() {
+		return studentname3;
+	}
+	public void setStudentname3(String studentname3) {
+		this.studentname3 = studentname3;
+	}
+	
+	
+}
