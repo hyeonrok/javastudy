9807c93fdc7dde0377c589db8ee6297ec2907a48
hyeonrok <wjsgusfhr@naver.com>
2024-02-22 21:17:24 +0900

    학생 평균과제3

---
+++ src/helloworld/SubjectTest.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class SubjectTest {
+
+	public static void main(String[] args) {
+
+		Constant constant = new Constant();
+		constant.setKorean("국어");
+		constant.setEnglish("영어");
+		constant.setMath("수학");
+		
+		constant.setStudentname1("Tony");
+		constant.setStudentname2("Jenny");
+		constant.setStudentname3("IU");
+	}
+}
